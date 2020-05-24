0a02aef3384405b8fe4140289e9e4d1c20735515
Frank Riley <fhriley@gmail.com>
2020-05-24 00:40:01 +0000

    bug fixes

--- src/domainindex.cc
+++ src/domainindex.cc
@@ -47,8 +47,8 @@ struct cmd_t
     {
         struct
         {
-            char name[256];
             bool count;
+            char name[256];
         } domain;
         struct
         {
@@ -59,11 +59,12 @@ struct cmd_t
 };
 
 static void* indexThread_(void*);
+static int findDomainID_(const char *domainString, const bool count);
 int findDomainID(const char *domainString, const bool count);
+static int findCacheID_(int domainID, int clientID);
 
 static std::map<std::string, int> domainsDataByString_;
 static std::map<cache_key_t, int> cacheByDomainAndClient_;
-static cmd_t cmd_;
 static pthread_t index_;
 static int socket_;
 static pid_t mainProcess_;
@@ -98,40 +99,57 @@ static size_t strlcpy(char * __restrict dst, const char * __restrict src, size_t
 	return(src - osrc - 1);	/* count does not include NUL */
 }
 
-void FTL_DomainIndexInit(void)
+int createSocket_(const char* path, struct sockaddr_un* addr)
 {
-    mainProcess_ = getpid();
-
-    socket_ = socket(AF_UNIX, SOCK_DGRAM | SOCK_CLOEXEC, 0);
-    if (socket_ < 0)
+    int sock = socket(AF_UNIX, SOCK_DGRAM | SOCK_CLOEXEC, 0);
+    if (sock < 0)
     {
         char buf[256];
         logg("Failed to create index socket: %s", strerror_r(errno, buf, sizeof(buf)));
         exit(EXIT_FAILURE);
     }
-   
-    memset(&server_, 0, sizeof(struct sockaddr_un));
-    server_.sun_family = AF_UNIX;
-    strlcpy(server_.sun_path, SERVER_SOCKET, sizeof(server_.sun_path));
-    server_.sun_path[0] = 0;
-   
-    if (bind(socket_, (const struct sockaddr *)&server_, sizeof(struct sockaddr_un)) < 0)
+
+    int enable = 1;
+    if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(enable)) < 0)
     {
-        close(socket_);
+        char buf[256];
+        logg("setsockopt failed: %s", strerror_r(errno, buf, sizeof(buf)));
+        exit(EXIT_FAILURE);
+    }
+   
+    addr->sun_family = AF_UNIX;
+    strlcpy(addr->sun_path, path, sizeof(addr->sun_path));
+    addr->sun_path[0] = 0;
+   
+    logg("binding to %s", &addr->sun_path[1]);
+    if (bind(sock, (struct sockaddr *)addr, sizeof(struct sockaddr_un)) < 0)
+    {
+        logg("closing sock");
+        close(sock);
         char buf[256];
         logg("Failed to bind index socket: %s", strerror_r(errno, buf, sizeof(buf)));
         exit(EXIT_FAILURE);
     }
-    index_ = create_thread_or_fail(indexThread_,  "index");
+
+    return sock;
 }
 
-static void* indexThread_(void*)
+static void send_(int sock, const void* data, int size, const struct sockaddr_un* addr, socklen_t addrLen)
 {
-    while (!killed)
+    logg("sending to %s", &addr->sun_path[1]);
+    if (sendto(sock, data, size, 0, (struct sockaddr *)addr, addrLen) < 0)
     {
-        struct sockaddr_un from;
-        socklen_t addrlen = sizeof(struct sockaddr_un);
-        ssize_t received = recvfrom(socket_, &cmd_, sizeof(cmd_), 0, (sockaddr *)&from, &addrlen);
+        char buf[256];
+        logg("FATAL: sendto() failed: %s", strerror_r(errno, buf, sizeof(buf)));
+		exit(EXIT_FAILURE);
+    }
+}
+
+static int recv_(int sock, void* data, int size, struct sockaddr_un* addr, socklen_t* addrLen)
+{
+    while (true)
+    {
+        ssize_t received = recvfrom(sock, data, size, 0, (struct sockaddr*)addr, addrLen);
         if (received < 0)
         {
             if (errno == EINTR || errno == EWOULDBLOCK)
@@ -139,98 +157,78 @@ static void* indexThread_(void*)
                 continue;
             }
             char buf[256];
-            logg("FATAL: index recvfrom() failed: %s", strerror_r(errno, buf, sizeof(buf)));
+            logg("FATAL: index fork recv() failed: %s", strerror_r(errno, buf, sizeof(buf)));
             exit(EXIT_FAILURE);
         }
+        return received;
+    }
+}
+
+void FTL_DomainIndexInit(void)
+{
+    mainProcess_ = getpid();
+    socket_ = createSocket_(SERVER_SOCKET, &server_);
+    //index_ = create_thread_or_fail(indexThread_,  "index");
+}
+
+static void* indexThread_(void*)
+{
+    cmd_t cmd;
+    while (!killed)
+    {
+        struct sockaddr_un from;
+        socklen_t addrlen = sizeof(struct sockaddr_un);
+        recv_(socket_, &cmd, sizeof(cmd), &from, &addrlen);
         
         int id = -1;
-        switch (cmd_.command)
+        switch (cmd.command)
         {
             case COMMAND_DOMAIN:
-                id = findDomainID(cmd_.u.domain.name, cmd_.u.domain.count);
+                logg("got domain: %s, count: %d", cmd.u.domain.name, cmd.u.domain.count);
+                id = findDomainID_(cmd.u.domain.name, cmd.u.domain.count);
                 logg("sending domain id %d", id);
                 break;
             case COMMAND_CACHE:
-                id = findCacheID(cmd_.u.cache.domainId, cmd_.u.cache.clientId);
+                logg("got domainId: %d, clientId: %d", cmd.u.cache.domainId, cmd.u.cache.clientId);
+                id = findCacheID_(cmd.u.cache.domainId, cmd.u.cache.clientId);
                 logg("sending cache id %d", id);
                 break;
             default:
-                logg("FATAL: index got invalid command: %d", cmd_.command);
+                logg("FATAL: index got invalid command: %d", cmd.command);
                 exit(EXIT_FAILURE);
         }
 
         // Send id back
-        if (sendto(socket_, &id, sizeof(id), 0, (struct sockaddr *)&from, addrlen) < 0)
-        {
-            char buf[256];
-            logg("FATAL: index sendto() failed: %s", strerror_r(errno, buf, sizeof(buf)));
-            exit(EXIT_FAILURE);
-        }
+        send_(socket_, &id, sizeof(id), &from, addrlen);
 
         logg("sent id %d", id);
     }
 
+    logg("closing server sock");
     close(socket_);
 
     return NULL;
 }
 
-static bool isForked()
+static bool isForked_()
 {
     return mainProcess_ != getpid();
 }
 
-static int getIdFromMainProcess_(int sendSize)
+static int getIdFromMainProcess_(cmd_t* cmd, int sendSize)
 {
-    int sock = socket(AF_UNIX, SOCK_DGRAM, 0);
-    if (sock < 0)
-    {
-        char buf[256];
-        logg("Failed to create fork index socket: %s", strerror_r(errno, buf, sizeof(buf)));
-        exit(EXIT_FAILURE);
-    }
+    logg("sendSize: %d", sendSize);
 
     struct sockaddr_un client;
-    memset(&client, 0, sizeof(struct sockaddr_un));
-    client.sun_family = AF_UNIX;
-    strlcpy(client.sun_path, CLIENT_SOCKET, sizeof(client.sun_path));
-    client.sun_path[0] = 0;
-   
-    if (bind(sock, (struct sockaddr *)&client, sizeof(struct sockaddr_un)) < 0)
-    {
-        close(sock);
-        char buf[256];
-        logg("Failed to bind fork index socket: %s", strerror_r(errno, buf, sizeof(buf)));
-        exit(EXIT_FAILURE);
-    }
+    int sock = createSocket_(CLIENT_SOCKET, &client);
 
-    if (sendto(sock, &cmd_, sendSize, 0, (struct sockaddr *)&server_, sizeof(struct sockaddr_un)) < 0)
-    {
-        close(sock);
-        char buf[256];
-        logg("FATAL: index fork sendto() failed: %s", strerror_r(errno, buf, sizeof(buf)));
-		exit(EXIT_FAILURE);
-    }
+    send_(sock, cmd, sendSize, &server_, sizeof(sa_family_t) + sizeof(SERVER_SOCKET) + 2);
 
     int id = -1;
-    while (true)
-    {
-        ssize_t received = recv(sock, &id, sizeof(id), 0);
-        if (received < 0)
-        {
-            if (errno == EINTR || errno == EWOULDBLOCK)
-            {
-                continue;
-            }
-            close(sock);
-            char buf[256];
-            logg("FATAL: index fork recv() failed: %s", strerror_r(errno, buf, sizeof(buf)));
-            exit(EXIT_FAILURE);
-        }
-        close(sock);
-        break;
-    }
+    recv_(sock, &id, sizeof(id), NULL, NULL);
 
+    logg("closing client sock");
+    close(sock);
     logg("received id %d", id);
     return id;
 }
@@ -250,6 +248,46 @@ static void incrementCount_(int domainId, bool count)
     }
 }
 
+static int findDomainID_(const char *domainString, const bool count)
+{
+    int domainID = counters->domains;
+    auto result = domainsDataByString_.insert(std::map<std::string, int>::value_type(domainString, domainID));
+    if (result.second)
+    {
+        // Didn't have it indexed, create a new entry
+        memory_check(DOMAINS);
+
+        domainsData* domain = getDomain(domainID, false);
+        if(domain == NULL)
+        {
+            //logg("ERROR: Encountered serious memory error in findDomainID()");
+            domainsDataByString_.erase(result.first);
+            unlock_shm();
+            return -1;
+        }
+
+        // Set magic byte
+        domain->magic = MAGICBYTE;
+        // Set its counter to 1 only if this domain is to be counted
+        // Domains only encountered during CNAME inspection are NOT counted here
+        domain->count = count ? 1 : 0;
+        // Set blocked counter to zero
+        domain->blockedcount = 0;
+        // Store domain name - no need to check for NULL here as it doesn't harm
+        domain->domainpos = addstr(domainString);
+        // Increase counter by one
+        counters->domains++;
+    }
+    else
+    {
+        // Already have it indexed
+        domainID = result.first->second;
+        incrementCount_(domainID,  count);
+    }
+
+    return domainID;
+}
+
 // A forked process has a copy of the main process's index. However, by the time the
 // forked process gets to this call, the local index could be out of sync. We rectify
 // this with the following procedure:
@@ -259,123 +297,109 @@ int findDomainID(const char *domainString, const bool count)
 {
     int domainID = -1;
 
-    if (isForked())
+    if (isForked_())
     {
+        // Check if we already have it locally
         auto result = domainsDataByString_.find(domainString);
+
         if (result == domainsDataByString_.end())
         {
-            // Didn't have it in local index, get it from the main process
-            cmd_.command = COMMAND_DOMAIN;
-            int len = strlcpy(cmd_.u.domain.name, domainString, sizeof(cmd_.u.domain.name));
-            cmd_.u.domain.count = count;
-            domainID = getIdFromMainProcess_(sizeof(cmd_.u.domain.count) + len);
+            // We don't have it, get it from the main process
+            cmd_t cmd;
+            cmd.command = COMMAND_DOMAIN;
+            int len = strlcpy(cmd.u.domain.name, domainString, sizeof(cmd.u.domain.name));
+            cmd.u.domain.count = count;
+            domainID = getIdFromMainProcess_(&cmd,
+                sizeof(cmd.command) + sizeof(cmd.u.domain) - sizeof(cmd.u.domain.name) + len + 1);
+
             // Insert it into local index
             domainsDataByString_.insert(std::map<std::string, int>::value_type(domainString, domainID));
         }
         else
         {
-            // Already have it in local index
+            // We do have it, return it
             domainID = result->second;
             incrementCount_(domainID,  count);
         }
     }
     else
     {
-        domainID = counters->domains;
-
-        auto result = domainsDataByString_.insert(std::map<std::string, int>::value_type(domainString, domainID));
-        if (result.second)
-        {
-            // Didn't have it indexed, create a new entry
-            memory_check(DOMAINS);
-
-            domainsData* domain = getDomain(domainID, false);
-            if(domain == NULL)
-            {
-                //logg("ERROR: Encountered serious memory error in findDomainID()");
-                unlock_shm();
-                return -1;
-            }
-
-            // Set magic byte
-            domain->magic = MAGICBYTE;
-            // Set its counter to 1 only if this domain is to be counted
-            // Domains only encountered during CNAME inspection are NOT counted here
-            domain->count = count ? 1 : 0;
-            // Set blocked counter to zero
-            domain->blockedcount = 0;
-            // Store domain name - no need to check for NULL here as it doesn't harm
-            domain->domainpos = addstr(domainString);
-            // Increase counter by one
-            counters->domains++;
-        }
-        else
-        {
-            // Already have it indexed
-            domainID = result.first->second;
-            incrementCount_(domainID,  count);
-        }
+        // Call the normal function
+        domainID = findDomainID_(domainString, count);
     }
 
     return domainID;
+}
+
+static int findCacheID_(int domainID, int clientID)
+{
+    // Get ID of new cache entry
+    int cacheID = counters->dns_cache_size;
+
+    cache_key_t key = {domainID, clientID};
+    auto result = cacheByDomainAndClient_.insert(std::map<cache_key_t, int>::value_type(key, cacheID));
+    if (result.second)
+    {
+        memory_check(DNS_CACHE);
+
+        DNSCacheData* dns_cache = getDNSCache(cacheID, false);
+        if (dns_cache == NULL)
+        {
+            //logg("ERROR: Encountered serious memory error in findCacheID()");
+            cacheByDomainAndClient_.erase(result.first);
+            return -1;
+        }
+
+        // Initialize cache entry
+        dns_cache->magic = MAGICBYTE;
+        dns_cache->blocking_status = UNKNOWN_BLOCKED;
+        dns_cache->domainID = domainID;
+        dns_cache->clientID = clientID;
+        dns_cache->force_reply = 0u;
+
+        // Increase counter by one
+        counters->dns_cache_size++;
+    }
+    else
+    {
+        cacheID = result.first->second;
+    }
+
+    return cacheID;
 }
 
 int findCacheID(int domainID, int clientID)
 {
     int cacheID = -1;
 
-    if (isForked())
+    if (isForked_())
     {
+        // Check if we already have it locally
         cache_key_t key = {domainID, clientID};
         auto result = cacheByDomainAndClient_.find(key);
+
         if (result == cacheByDomainAndClient_.end())
         {
-            // Didn't have it in local index, get it from the main process
-            cmd_.command = COMMAND_CACHE;
-            cmd_.u.cache.domainId = domainID;
-            cmd_.u.cache.clientId = clientID;
-            cacheID = getIdFromMainProcess_(sizeof(cmd_.u.cache));
-            // Insert it into local index
+            // We don't have it, get it from the main process
+            cmd_t cmd;
+            cmd.command = COMMAND_CACHE;
+            cmd.u.cache.domainId = domainID;
+            cmd.u.cache.clientId = clientID;
+            cacheID = getIdFromMainProcess_(&cmd, sizeof(cmd.command) + sizeof(cmd.u.cache));
+
+            // Insert it into our local index
             cacheByDomainAndClient_.insert(std::map<cache_key_t, int>::value_type(key, cacheID));
         }
         else
         {
-            // Already have it in local index
+            // We do have it, return it
             cacheID = result->second;
         }
     }
     else
     {
-        // Get ID of new cache entry
-        cacheID = counters->dns_cache_size;
-
-        cache_key_t key = {domainID, clientID};
-        auto result = cacheByDomainAndClient_.insert(std::map<cache_key_t, int>::value_type(key, cacheID));
-        if (result.second)
-        {
-            memory_check(DNS_CACHE);
-
-            DNSCacheData* dns_cache = getDNSCache(cacheID, false);
-            if (dns_cache == NULL)
-            {
-                //logg("ERROR: Encountered serious memory error in findCacheID()");
-                return -1;
-            }
-
-            // Initialize cache entry
-            dns_cache->magic = MAGICBYTE;
-            dns_cache->blocking_status = UNKNOWN_BLOCKED;
-            dns_cache->domainID = domainID;
-            dns_cache->clientID = clientID;
-            dns_cache->force_reply = 0u;
-
-            // Increase counter by one
-            counters->dns_cache_size++;
-        }
-        else
-        {
-            cacheID = result.first->second;
-        }
+        // Call the normal function
+        cacheID = findCacheID_(domainID, clientID);
     }
 
     return cacheID;
